27b1c34ba3f00173344f35e1b20e34179c429585
arneson205037 <35339062+arneson205037@users.noreply.github.com>
2019-02-13 12:11:47 -0500

    Update Controls.java

--- MazeProject/app/src/main/java/com/example/a300671/mazeproject/Controls.java
+++ MazeProject/app/src/main/java/com/example/a300671/mazeproject/Controls.java
@@ -1,19 +1,33 @@
 package com.example.a300671.mazeproject;
 
+import android.content.res.Resources;
 import android.widget.Button;
 
 public class Controls
 {
-    DemoView view;
+    static myButton moveButton;
+    static myButton rightButton;
+    static myButton leftButton;
 
-    myButton moveButton;
-    myButton rightButton;
-    myButton leftButton;
-
-    public static void pressButton(int buttonId)
+    public Controls()
     {
-
+        moveButton = new myButton((int) (Resources.getSystem().getDisplayMetrics().widthPixels * 0.45), (int) (Resources.getSystem().getDisplayMetrics().heightPixels * 0.1));
+        rightButton = new myButton((int) (Resources.getSystem().getDisplayMetrics().widthPixels * 0.55), (int) (Resources.getSystem().getDisplayMetrics().heightPixels * 0.7));
+        leftButton = new myButton((int) (Resources.getSystem().getDisplayMetrics().widthPixels * 0.15), (int) (Resources.getSystem().getDisplayMetrics().heightPixels * 0.7));
     }
+
+    public static myButton[] getButtons()
+    {
+        myButton[] set = {moveButton, rightButton, leftButton};
+        return set;
+    }
+
+    public void pressButton(int num)
+    {
+        myButton[] set = {moveButton, rightButton, leftButton};
+        set[num].press();
+    }
+
 
 
 }
